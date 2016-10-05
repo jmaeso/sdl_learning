209a598216db0f48f9140a2706b456f6b474cd46
Joan Maeso <jmaeso@enti.cat>
2016-10-05 14:01:56 +0200

    Added some button attributes

--- source/Button.h
+++ source/Button.h
@@ -1,10 +1,21 @@
 #pragma once
 
+#include <string>
 #include "Sprite.h"
+#include "SDL2/SDL_TTF.h"
 
 class Button {
 public:
-	Sprite Sprite;
+	Sprite sprite;
+	SDL_Rect rect;
+	SDL_Color buttonColor;
+	SDL_Color textColor;
+	std::string text;
+
+	Button();
+	Button(int x, int y, int width, int height, SDL_Color color);
+	Button(int x, int y, Sprite sprite);
+
 
 	bool isHover(int cursorX, int cursorY);
 }
